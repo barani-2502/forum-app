c30dfc60c4f858d5ae5179590159c8a618a35071
barani-2502 <barani@testpress.in>
2025-10-08 15:50:36 +0530

    Split form template and added into includes folder

--- boards/tests.py
+++ boards/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.urls import resolve
 from .views import home, board_topics, new_topic
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 class HomeTests(TestCase):
     def setUp(self):
@@ -108,3 +109,16 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+
+    def test_contains_form(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
+    def test_new_topic_invalid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
